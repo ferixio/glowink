507e5dc798bd0f0ed7b782d8903ed23ca4b394f9
zuwhi <zuwhyyy@gmail.com>
2025-07-22 08:41:30 +0700

    Update UserPanelProvider to use CustomLogin for user authentication in Filament panel.

---
+++ app/Filament/Auth/CustomLogin.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Filament\Auth;
+use Filament\Forms\Form;
+
+use Filament\Forms\Components\Component;
+use Filament\Forms\Components\TextInput;
+use Filament\Pages\Auth\Login;
+
+class CustomLogin extends Login
+{
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                // $this->getEmailFormComponent(),
+                $this->getLoginFormComponent(),
+                $this->getPasswordFormComponent(),
+                $this->getRememberFormComponent(),
+            ])
+            ->statePath('data');
+    }
+
+    protected function getLoginFormComponent(): Component
+    {
+        return TextInput::make('login')
+            ->label('Login')
+            ->required()
+            ->autocomplete()
+            ->autofocus()
+            ->extraInputAttributes(['tabindex' => 1]);
+    }
+    protected function getCredentialsFromFormData(array $data): array
+    {
+        $login_type = filter_var($data['login'], FILTER_VALIDATE_EMAIL) ? 'email' : 'id_mitra';
+
+        return [
+            $login_type => $data['login'],
+            'password' => $data['password'],
+        ];
+    }
+}
--- app/Providers/Filament/UserPanelProvider.php
+++ app/Providers/Filament/UserPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Auth\CustomLogin;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -26,7 +27,7 @@ class UserPanelProvider extends PanelProvider
         return $panel
             ->id('user')
             ->path('user')
-            ->login()
+            ->login(CustomLogin::class)
             ->brandName("Glowink")
             ->colors([
                 'primary' => Color::Amber,
